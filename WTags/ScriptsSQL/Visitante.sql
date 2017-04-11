7a892fccefe48e26ebedc5834193f2a5e8f38323
Bruno Vitalino <brunovitalino@hotmail.com>
2017-04-11 17:22:53 -0300

    Primeiro commit. Pasta com os ScriptsSQL adicionada.
    Signed-off-by: Bruno Vitalino <brunovitalino@hotmail.com>

---
+++ WTags/ScriptsSQL/Visitante.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE wtags;
+USE wtags;
+
+CREATE TABLE Visitante
+(
+	id INT(6) UNSIGNED ZEROFILL AUTO_INCREMENT,
+    nome VARCHAR(45) NOT NULL,
+    telefone VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO Visitante(nome, telefone) VALUES ('Bruno Vitalino', '99998888');
+INSERT INTO Visitante(nome, telefone) VALUES ('Sandra Regina', '99998877');
+INSERT INTO Visitante(nome, telefone) VALUES ('Breno Oliveira', '99998866');
+
+SELECT * FROM Visitante;
+
+DROP TABLE Visitante;
