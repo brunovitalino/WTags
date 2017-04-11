925a7daa0e1a0fa4de13c072c4146bf3beae38e4
Bruno Vitalino <brunovitalino@hotmail.com>
2017-04-11 17:57:46 -0300

    Criada e implementada a classe de conexão ao db, sua biblioteca foi adicionada também.
    Signed-off-by: Bruno Vitalino <brunovitalino@hotmail.com>

---
+++ WTags/src/model/ConexaoFactory.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoFactory
+{
+	
+	public Connection getConexao()
+	{
+		Connection conexao = null;
+		
+		try {
+			Class.forName("com.sql.jdbc.Driver");
+			try
+			{
+				conexao = DriverManager.getConnection("jdbc:mysql:localhost/wtags", "root", "123");
+			}
+			catch (SQLException e)
+			{
+				//e.printStackTrace();
+				System.out.println("ERRO2. Nao foi possivel se conectar.");
+				throw new RuntimeException(e);
+			}
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("ERRO1. Conector nao localizado");
+			e1.printStackTrace();
+		}
+		
+		return conexao;
+	}
+
+}
